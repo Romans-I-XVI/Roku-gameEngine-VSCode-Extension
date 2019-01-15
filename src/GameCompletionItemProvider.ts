e848bcc752876891f3e5c170ebea75ebb60bce54
asojka09 <austinsojka@gmail.com>
2019-01-15 16:37:24 -0600

    Added the rest of the game engine methods

--- src/GameCompletionItemProvider.ts
+++ src/GameCompletionItemProvider.ts
@@ -270,6 +270,258 @@ export default class GameCompletionItemProvider implements CompletionItemProvide
                     `Draws safe zones. Needs to be refactored so it's easier to draw on top layer above all other images.`
                 )
             },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'Pause',
+                insertText: new vscode.SnippetString('Pause()'),
+                detail: 'Pause() as Void',
+                documentation: new vscode.MarkdownString(
+                    `Pauses the game, when game is paused and if an entity is pauseable=true (default) only onDrawBegin and onDrawEnd methods are called, as well as internal image drawing.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'Resume',
+                insertText: new vscode.SnippetString('Resume()'),
+                detail: 'Resume() as Dynamic',
+                documentation: new vscode.MarkdownString(
+                    `Resumes the game if paused.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'isPaused',
+                insertText: new vscode.SnippetString('isPaused()'),
+                detail: 'isPaused() as Boolean',
+                documentation: new vscode.MarkdownString(
+                    `Returns true if the game is paused.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'setBackgroundColor',
+                insertText: new vscode.SnippetString('setBackgroundColor(${1:color as Integer})'),
+                detail: 'setBackgroundColor(color as Integer) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Sets the color to be used in the Clear() method on the canvas/screen.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'getDeltaTime',
+                insertText: new vscode.SnippetString('getDeltaTime()'),
+                detail: 'getDeltaTime() as Float',
+                documentation: new vscode.MarkdownString(
+                    `Returns the delta time that was calculated at the beginning of the current game loop.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'getRoom',
+                insertText: new vscode.SnippetString('getRoom()'),
+                detail: 'getRoom() as Object',
+                documentation: new vscode.MarkdownString(
+                    `Returns the current room object instance.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'getCanvas',
+                insertText: new vscode.SnippetString('getCanvas()'),
+                detail: 'getCanvas() as Object',
+                documentation: new vscode.MarkdownString(
+                    `Returns the current game canvas. This will be an roBitmap unless canvas_as_screen_if_possible is set, in which case it could potentially return the roScreen object.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'getScreen',
+                insertText: new vscode.SnippetString('getScreen()'),
+                detail: 'getScreen() as Object',
+                documentation: new vscode.MarkdownString(
+                    `Returns the current roScreen object.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'resetScreen',
+                insertText: new vscode.SnippetString('resetScreen()'),
+                detail: 'resetScreen() as Void',
+                documentation: new vscode.MarkdownString(
+                    `This is available because any time other screen components are used it can potentially cause glitching of the current roScreen object. As of January 2019 all other screen components have been removed however there still appears to be issues with roScreen when using the Roku_Ads library. As it stands resetScreen should still be called any time an ad is shown.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'defineObject',
+                insertText: new vscode.SnippetString('defineObject(${1:object_name as String}, ${2:object_creation_function as Function})'),
+                detail: 'defineObject(object_name as String, object_creation_function as Function) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Define a new game object. The object_creation_function should be a function that accepts and modifies the incoming object instance whenever a new instance is spawned.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'getInstanceByID',
+                insertText: new vscode.SnippetString('getInstanceByID(${1:instance_id as String})'),
+                detail: 'getInstanceByID(instance_id as String) as Dynamic',
+                documentation: new vscode.MarkdownString(
+                    `Returns the instance with the given ID if it exists, otherwise returns invalid.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'destroyInstance',
+                insertText: new vscode.SnippetString('destroyInstance(${1:instance as Object}${2: [, call_onDestroy = true]})'),
+                detail: 'destroyInstance(instance as Object, call_onDestroy = true as Boolean) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Immediately destroys the given object instance, note that this instantly clears all data in that instance so any subsequent references are potentially breaking. If call_onDestroy is true the object's onDestroy method will be called before destruction occurs.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'defineRoom',
+                insertText: new vscode.SnippetString('defineRoom(${1:room_name as String}, ${2:room_creation_function as Function})'),
+                detail: 'defineRoom(room_name as String, room_creation_function as Function) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Define a new game room. Rooms have the same methods available to them as objects and should be defined in the same way.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'resetRoom',
+                insertText: new vscode.SnippetString('resetRoom()'),
+                detail: 'resetRoom() as Void',
+                documentation: new vscode.MarkdownString(
+                    `Resets the current room. The args roAssociativeArray that was passed in originally will be passed in again so arguments stay the same as when this room was originally switched to.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'loadBitmap',
+                insertText: new vscode.SnippetString('loadBitmap(${1:bitmap_name as String}, ${2:path as Dynamic})'),
+                detail: 'loadBitmap(bitmap_name as String, path as Dynamic) as Boolean',
+                documentation: new vscode.MarkdownString(
+                    `Loads the bitmap in to memory and makes it accessible via getBitmap. Path can optionally be an roAssociativeArray of {width: width as Integer, height: height as Integer, AlphaEnabled: enabled as Boolean} to create a new empty bitmap of the given width/height. Returns true if successful.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'unloadBitmap',
+                insertText: new vscode.SnippetString('unloadBitmap(${1:bitmap_name as String})'),
+                detail: 'unloadBitmap(bitmap_name as String) as Boolean',
+                documentation: new vscode.MarkdownString(
+                    `Removes a previoulsy loaded bitmap. Note that freeing of allocated memory might not occur immediately. Returns false if no such bitmap was previously loaded.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'registerFont',
+                insertText: new vscode.SnippetString('registerFont(${1:path as String})'),
+                detail: 'registerFont(path as String) as Boolean',
+                documentation: new vscode.MarkdownString(
+                    `Registers a new font from the given path. Note that all fonts placed within the pkg:/fonts/ folder will automatically be registered at runtime.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'loadFont',
+                insertText: new vscode.SnippetString('loadFont(${1:font_name as String}, ${2:font as String}, ${3:size as Integer}, ${4:italic as Boolean}, ${5:bold as Boolean})'),
+                detail: 'loadFont(font_name as String, font as String, size as Integer, italic as Boolean, bold as Boolean) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Loads a previously registered \"font\" with the given size, italic, and bold options. This makes the loaded font accessible via getFont by \"font_name\".`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'unloadFont',
+                insertText: new vscode.SnippetString('unloadFont(${1:font_name as String})'),
+                detail: 'unloadFont(font_name as String) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Unloades a font previously loaded with loadFont.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'musicPlay',
+                insertText: new vscode.SnippetString('musicPlay(${1:path as String}${2: [, loop = false]})'),
+                detail: 'musicPlay(path as String, loop = false as Boolean) as Boolean',
+                documentation: new vscode.MarkdownString(
+                    `Plays a song from the given path. Returns false if file doesn't exist.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'musicStop',
+                insertText: new vscode.SnippetString('musicStop()'),
+                detail: 'musicStop() as Void',
+                documentation: new vscode.MarkdownString(
+                    `Stops the currently playing music.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'musicPause',
+                insertText: new vscode.SnippetString('musicPause()'),
+                detail: 'musicPause() as Void',
+                documentation: new vscode.MarkdownString(
+                    `Pauses the currently playing music.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'musicResume',
+                insertText: new vscode.SnippetString('musicResume()'),
+                detail: 'musicResume() as Void',
+                documentation: new vscode.MarkdownString(
+                    `Resumes the currently paused music.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'loadSound',
+                insertText: new vscode.SnippetString('loadSound(${1:sound_name as String}, ${2:path as String})'),
+                detail: 'loadSound(sound_name as String, path as String) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Loads a sound file as a roAudioResource`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'newAsyncUrlTransfer',
+                insertText: new vscode.SnippetString('newAsyncUrlTransfer()'),
+                detail: 'newAsyncUrlTransfer() as Object',
+                documentation: new vscode.MarkdownString(
+                    `Returns a new roUrlTransfer object to be used for async operations. When a roUrlEvent occurs on this roUrlTransfer object the event will be passed in to the onUrlEvent of all active instances, the roUrlTransfer object will then be removed.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'setInputInstance',
+                insertText: new vscode.SnippetString('setInputInstance(${1:instance as Object})'),
+                detail: 'setInputInstance(instance as Object) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Sets an object instance as the current input handler. When input instance is set no other instances will receive onButton calls.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'unsetInputInstance',
+                insertText: new vscode.SnippetString('unsetInputInstance()'),
+                detail: 'unsetInputInstance() as Void',
+                documentation: new vscode.MarkdownString(
+                    `Unsets the current input instance if one was previously set. This makes it so onButton calls will resume normal behavior.`
+                )
+            },
+            {
+                kind: CompletionItemKind.Method,
+                label: 'postGameEvent',
+                insertText: new vscode.SnippetString('postGameEvent(${1:event as String}${2: [, data as Object]})'),
+                detail: 'postGameEvent(event as String, data = {} as Object) as Void',
+                documentation: new vscode.MarkdownString(
+                    `Immediately calls onGameEvent on all active instances with the given event string and optional data object.`
+                )
+            }
         ];
     }
 }
