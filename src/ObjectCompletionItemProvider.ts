f469d3176754962f1f591646f2be98b7f8e7aea2
asojka09 <austinsojka@gmail.com>
2019-01-16 11:08:16 -0600

    Setup object structure

--- src/ObjectCompletionItemProvider.ts
+++ src/ObjectCompletionItemProvider.ts
@@ -21,15 +21,43 @@ export default class ObjectCompletionItemProvider implements CompletionItemProvi
         let precededByChars = (linePrefix.match(new RegExp('([a-z|0-9|_])object\\.', 'i')) !== null);
         let partOfGameObject = (document.getText().match(new RegExp('object\\.onCreate', 'i')) !== null);
 
+
+
         if (endsWithObject && !precededByChars && partOfGameObject) {
-            completionItems.push(
-                {
-                    kind: CompletionItemKind.Method,
-                    label: "testing"
+            let existingOverrideMethods = this.getExistingOverrideMethods(document);
+
+            for (let key in existingOverrideMethods) {
+                let exists = existingOverrideMethods[key];
+                if (!exists) {
+                    completionItems.push(this.completionItemsDictionary[key]);
                 }
-            );
+            }
         }
 
         return completionItems;
+    }
+
+    private getExistingOverrideMethods(document: TextDocument): { [key: string]: boolean } {
+        let existingOverrideMethods: { [key: string]: boolean } = {}
+        for (let key in this.completionItemsDictionary) {
+            existingOverrideMethods[key] = false;
+        }
+
+        for (let key in existingOverrideMethods) {
+            let regex = new RegExp('object\\.' + key + '(.*)=(.*)function', 'i')
+            let exists = (document.getText().match(regex) !== null);
+            if (exists) {
+                existingOverrideMethods[key] = true;
+            }
+        }
+
+        return existingOverrideMethods;
+    }
+
+    private completionItemsDictionary: { [key: string]: CompletionItem } = {
+        onUpdate: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onUpdate'
+        }
     }
 }
