8fd8528eb96d4b8db57f1a13be761fb0d7e1308c
Austin <austinsojka@gmail.com>
2019-01-17 14:07:41 -0600

    Added all object override method snippets

--- src/ObjectCompletionItemProvider.ts
+++ src/ObjectCompletionItemProvider.ts
@@ -67,6 +67,177 @@ export default class ObjectCompletionItemProvider implements CompletionItemProvi
             documentation: new vscode.MarkdownString(
                 `The main update loop for an entity with delta time passed in.`
             )
+        },
+        onPreCollision: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onPreCollision',
+            insertText: new vscode.SnippetString(
+                'onPreCollision = function()\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onPreCollision()',
+            documentation: new vscode.MarkdownString(
+                `Called just before collision checking occurs. Can be used to make adjustments before handling collisions.`
+            )
+        },
+        onCollision: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onCollision',
+            insertText: new vscode.SnippetString(
+                'onCollision = function(collider_name as String, other_collider_name as String, other_instance as Object)\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onCollision(collider_name as String, other_collider_name as String, other_instance as Object)',
+            documentation: new vscode.MarkdownString(
+                `Called whenever an entity collision occurs, may be called multiple times per update loop based on collisions with multiple entities and colliders.`
+            )
+        },
+        onPostCollision: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onPostCollision',
+            insertText: new vscode.SnippetString(
+                'onPostCollision = function()\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onPostCollision()',
+            documentation: new vscode.MarkdownString(
+                `Called just after collision checking occurs. Can be used to make adjustments after handling collisions.`
+            )
+        },
+        onDrawBegin: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onDrawBegin',
+            insertText: new vscode.SnippetString(
+                'onDrawBegin = function(canvas as Object)\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onDrawBegin(canvas as Object)',
+            documentation: new vscode.MarkdownString(
+                `Called before drawing images that were added with addImage. This can be used to do drawing underneath those images.`
+            )
+        },
+        onDrawEnd: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onDrawEnd',
+            insertText: new vscode.SnippetString(
+                'onDrawEnd = function(canvas as Object)\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onDrawEnd(canvas as Object)',
+            documentation: new vscode.MarkdownString(
+                `Called after drawing images that were added with addImage. This can be used to do drawing above those images.`
+            )
+        },
+        onButton: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onButton',
+            insertText: new vscode.SnippetString(
+                'onButton = function(code as Integer)\n' +
+                '\t$0\n' +
+                '\n' +
+                '\t\' -------Button Code Reference--------\n' +
+                '\t\' Pressed | Released | Held\n' +
+                '\n' +
+                '\t\' Back  0  100 1000\n' +
+                '\t\' Up  2  102 1002\n' +
+                '\t\' Down  3  103 1003\n' +
+                '\t\' Left  4  104 1004\n' +
+                '\t\' Right  5  105 1005\n' +
+                '\t\' Select  6  106 1006\n' +
+                '\t\' Instant Replay  7  107 1007\n' +
+                '\t\' Rewind  8  108 1008\n' +
+                '\t\' Fast  Forward  9  109 1009\n' +
+                '\t\' Info  10  110 1010\n' +
+                '\t\' Play  13  113 1013\n' +
+                'end function'
+            ),
+            detail: 'onButton(code as Integer)',
+            documentation: new vscode.MarkdownString(
+                `Called when a button is pressed, held or released. Note that on the Roku only one button can ever be pressed at a time.`
+            )
+        },
+        onECPKeyboard: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onECPKeyboard',
+            insertText: new vscode.SnippetString(
+                'onECPKeyboard = function(char as String)\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onECPKeyboard(char as String)',
+            documentation: new vscode.MarkdownString(
+                `Called when ECP Keyboard key input has occured.`
+            )
+        },
+        onAudioEvent: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onAudioEvent',
+            insertText: new vscode.SnippetString(
+                'onAudioEvent = function(msg as Object)\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onAudioEvent(msg as Object)',
+            documentation: new vscode.MarkdownString(
+                `Called when an audio player event occured. msg will be of type roAudioPlayerEvent.`
+            )
+        },
+        onUrlEvent: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onUrlEvent',
+            insertText: new vscode.SnippetString(
+                'onUrlEvent = function(msg as Object)\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onUrlEvent(msg as Object)',
+            documentation: new vscode.MarkdownString(
+                `Called when a url event occured. msg will be of type roUrlEvent.`
+            )
+        },
+        onGameEvent: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onGameEvent',
+            insertText: new vscode.SnippetString(
+                'onGameEvent = function(event as String, data as Object)\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onGameEvent(event as String, data as Object)',
+            documentation: new vscode.MarkdownString(
+                `Called immediately on all entities when postGameEvent was triggered.`
+            )
+        },
+        onChangeRoom: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onChangeRoom',
+            insertText: new vscode.SnippetString(
+                'onChangeRoom = function(new_room_name as String)\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onChangeRoom(new_room_name as String)',
+            documentation: new vscode.MarkdownString(
+                `Called immediately on all entities when changeRoom was triggered.`
+            )
+        },
+        onDestroy: {
+            kind: CompletionItemKind.Snippet,
+            label: 'onDestroy',
+            insertText: new vscode.SnippetString(
+                'onDestroy = function()\n' +
+                '\t$0\n' +
+                'end function'
+            ),
+            detail: 'onDestroy()',
+            documentation: new vscode.MarkdownString(
+                `Called just before an entity gets destroyed and removed from the game loop.`
+            )
         }
     };
 }
